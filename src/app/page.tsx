2ce475cefc2e6039c5b05a225a654435b60eb93e
Ellah Nzikoba <ellah.nzikoba@toasttab.com>
2024-10-08 18:00:02 -0700

    Fix bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,18 @@ const Home = () => {
         in San Francisco, CA.
         <br />
         <br />
-        Growing up with parents in the medical professions,I studied
-        Biochemistry at Tufts University. However, having had a passion for art
-        and design growing up, I would always find myself on the internet
-        teaching myself how to code a website, effectively igniting a love for
+        In 2019, I completed in Bachelor of Science in Biochemistry at
+        Tufts University. However, having had a passion for art and design
+        growing up, I would always find myself on the web teaching myself how to
+        design and code websits and apps, effectively igniting a love for
         programming within me.
         <br />
         <br />
         After college, it became very clear to me that research and medicine was
         not the field for me and I had grown a desire to learn more about
-        computers and programming. In 2023, I completed my Master’s in Computer
-        Science and now I am on a journey to grow as an engineer and designer.
+        computers and programming. In 2023, I completed my Master&apos;s in
+        Computer Science and now I am on a journey to grow as an engineer and
+        designer.
       </p>
     </div>
   )
